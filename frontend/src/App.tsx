b4c6a8d612ae80cd4ae8502b84dfdedbed37500f
alexisljn <alexis.lejeune@gmx.fr>
2022-12-23 14:54:02 +0100

    grid layout on App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
+import Header from "./components/header/Header";
 
 function App() {
-  return (
-    <div>
-      VOTING
-    </div>
-  );
+    return (
+        <div className="grid">
+            <Header/>
+            {/*Routing */}
+            <div className="sidebar">SIDEBAR</div>
+            <div className="content">CONTENT</div>
+        </div>
+    );
 }
 
 export default App;

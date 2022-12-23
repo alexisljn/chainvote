d10735c720fe38fa27a51cad690e4c28498a56ad
alexisljn <alexis.lejeune@gmx.fr>
2022-12-23 14:53:36 +0100

    create Nav Component

---
+++ frontend/src/components/header/Nav.tsx
@@ -0,0 +1,13 @@
+function Nav() {
+    return (
+        <nav className="header-nav">
+            <ul className="nav-links">
+                <li><a href="#">Home</a></li>
+                <li><a href="#">History</a></li>
+                <li><a href="#">Admin</a></li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Nav;

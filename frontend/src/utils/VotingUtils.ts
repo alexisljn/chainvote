dcc478e638c5399d224adb26aef057254cc3fa9d
alexisljn <alexis.lejeune@gmx.fr>
2022-12-24 19:50:24 +0100

    create generateStatusesList()

--- frontend/src/utils/VotingUtils.ts
+++ frontend/src/utils/VotingUtils.ts
@@ -1,7 +1,26 @@
 import {ethers, providers} from "ethers";
 import VOTING_JSON from "../artifacts/contracts/voting.sol/Voting.json";
+
 function getVotingContractInstance(provider: providers.Web3Provider) {
     return new ethers.Contract(process.env.REACT_APP_CONTRACT_ADDRESS!, VOTING_JSON.abi, provider.getSigner());
 }
 
-export {getVotingContractInstance}
+function generateStatusesList(currentStatus: number) {
+    const statusLabels = [
+        "Voters registration",
+        "Proposals registration",
+        "Proposals registration ended",
+        "Voting session",
+        "Voting session ended",
+        "Equality",
+        "Votes tallied"
+    ];
+
+    if (currentStatus !== 5) {
+        statusLabels.splice(5, 1);
+    }
+
+    return statusLabels;
+}
+
+export {getVotingContractInstance, generateStatusesList}

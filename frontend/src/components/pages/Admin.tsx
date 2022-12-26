9a157642f5cc60507e58938c6cc2434d3525679a
alexisljn <alexis.lejeune@gmx.fr>
2022-12-26 15:51:58 +0100

    Get voting status and display components based on it

--- frontend/src/components/pages/Admin.tsx
+++ frontend/src/components/pages/Admin.tsx
@@ -1,11 +1,23 @@
 import {useContext, useEffect, useState} from "react";
 import {ChainVoteContext} from "../../App";
+import VotersRegistration from "../admin/VotersRegistration";
+import {VotingStatus} from "../../utils/VotingUtils";
 
 function Admin() {
 
-    const {permissions} = useContext(ChainVoteContext);
+    const {permissions, votingContract} = useContext(ChainVoteContext);
+
+    const [votingStatus, setVotingStatus] = useState<number | null>(null);
 
     const [isLoading, setIsLoading] = useState<boolean>(true);
+
+    useEffect(() => {
+        if (!votingContract) return;
+
+        (async () => {
+           setVotingStatus(await votingContract.getStatus());
+        })()
+    }, [votingContract])
 
     useEffect(() => {
         const {isOwner} = permissions;
@@ -16,16 +28,28 @@ function Admin() {
         }
 
         setIsLoading(false);
-    }, [permissions])
+    }, [permissions]);
 
-    if (isLoading) {
+    if (isLoading || votingStatus === null) {
         return (
             <p>Loading...</p>
         )
     }
 
+    switch (votingStatus) {
+        case VotingStatus.RegisteringVoters:
+            return (
+                <div>
+                    <h1 className="top-grid-area-element">Voting administration</h1>
+                    <VotersRegistration/>
+                </div>
+            )
+    }
+
     return (
-        <p>Admin</p>
+        <div>
+            Error
+        </div>
     )
 }
 

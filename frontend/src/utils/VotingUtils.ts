d141d7b17f17b428454fc066bcc9f9b0ee73396b
alexisljn <alexis.lejeune@gmx.fr>
2022-12-26 09:13:28 +0100

    pass provider instead of signer to contract in order to use contract view functions without connecting wallet

--- frontend/src/utils/VotingUtils.ts
+++ frontend/src/utils/VotingUtils.ts
@@ -2,7 +2,7 @@ import {ethers, providers} from "ethers";
 import VOTING_JSON from "../artifacts/contracts/voting.sol/Voting.json";
 
 function getVotingContractInstance(provider: providers.Web3Provider) {
-    return new ethers.Contract(process.env.REACT_APP_CONTRACT_ADDRESS!, VOTING_JSON.abi, provider.getSigner());
+    return new ethers.Contract(process.env.REACT_APP_CONTRACT_ADDRESS!, VOTING_JSON.abi, provider);
 }
 
 function generateStatusesList(currentStatus: number) {

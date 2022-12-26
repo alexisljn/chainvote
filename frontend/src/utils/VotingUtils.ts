3694cd9bc050559d823f450bb118ffe73067a708
alexisljn <alexis.lejeune@gmx.fr>
2022-12-26 10:32:40 +0100

    Create functions to get user permissions on contract

--- frontend/src/utils/VotingUtils.ts
+++ frontend/src/utils/VotingUtils.ts
@@ -1,11 +1,25 @@
-import {ethers, providers} from "ethers";
+import {Contract, ethers, providers} from "ethers";
 import VOTING_JSON from "../artifacts/contracts/voting.sol/Voting.json";
 
-function getVotingContractInstance(provider: providers.Web3Provider) {
+function getVotingContractInstance(provider: providers.Web3Provider): Contract {
     return new ethers.Contract(process.env.REACT_APP_CONTRACT_ADDRESS!, VOTING_JSON.abi, provider);
 }
 
-function generateStatusesList(currentStatus: number) {
+async function isOwner(votingContract: Contract, address: string): Promise<boolean> {
+    const owner: string = await votingContract.owner();
+
+    return owner === address;
+}
+
+async function canVote(votingContract: Contract): Promise<boolean> {
+    return await votingContract.canVote();
+}
+
+async function canRegisterItself(votingContract: Contract): Promise<boolean> {
+    return await votingContract.canRegisterItself();
+}
+
+function generateStatusesList(currentStatus: number): string[] {
     const statusLabels = [
         "Voters registration",
         "Proposals registration",
@@ -23,4 +37,4 @@ function generateStatusesList(currentStatus: number) {
     return statusLabels;
 }
 
-export {getVotingContractInstance, generateStatusesList}
+export {getVotingContractInstance, isOwner, canVote, canRegisterItself, generateStatusesList}

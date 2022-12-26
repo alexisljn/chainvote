49bde016c9c9bc2f20325391a0096a8b4e5519b6
alexisljn <alexis.lejeune@gmx.fr>
2022-12-26 10:34:01 +0100

    display Admin link only if user is owner of contract

--- frontend/src/components/header/Nav.tsx
+++ frontend/src/components/header/Nav.tsx
@@ -1,12 +1,21 @@
 import {NavLink} from "react-router-dom";
+import {useContext} from "react";
+import {ChainVoteContext} from "../../App";
 
 function Nav() {
+
+    const {permissions} = useContext(ChainVoteContext);
+
+    const {isOwner} = permissions;
+
     return (
         <nav className="header-nav">
             <ul className="nav-links">
                 <li><NavLink to="/">Home</NavLink></li>
                 <li><NavLink to="history">History</NavLink></li>
-                <li><NavLink to="/admin">Admin</NavLink></li>
+                {isOwner &&
+                    <li><NavLink to="/admin">Admin</NavLink></li>
+                }
             </ul>
         </nav>
     )

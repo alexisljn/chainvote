783fca03cd5ed837cc125ac41550d6258f0d781e
alexisljn <alexis.lejeune@gmx.fr>
2022-12-28 08:45:45 +0100

    create workflowStatusChangeHandler()

--- frontend/src/events-manager/VotingEventsManager.ts
+++ frontend/src/events-manager/VotingEventsManager.ts
@@ -3,14 +3,34 @@ import {Contract} from "ethers";
 const CONTRACT_EVENT = "contractEvent"
 function listenContractEvents(votingContract: Contract) {
     votingContract.on('VoterRegistered', voterRegisteredHandler);
+
+    votingContract.on('WorkflowStatusChange', workflowStatusChangeHandler);
 }
 
 function cleanContractEvents(votingContract: Contract) {
     votingContract.off('VoterRegistered', voterRegisteredHandler);
+
+    votingContract.off('WorkflowStatusChange', workflowStatusChangeHandler);
 }
 
 function voterRegisteredHandler(voterAddress: string, caller: string) {
-    const event = new CustomEvent(CONTRACT_EVENT, {detail: {type: 'voterRegistered', value: {voterAddress, caller}}});
+    const event = new CustomEvent(CONTRACT_EVENT, {
+        detail: {
+            type: 'voterRegistered',
+            value: {voterAddress, voterRegisteredCaller: caller}
+        }
+    });
+
+    window.dispatchEvent(event);
+}
+
+function workflowStatusChangeHandler(previousStatus: number, newStatus: number, caller: string) {
+    const event = new CustomEvent(CONTRACT_EVENT, {
+        detail: {
+            type: 'workflowStatusChange',
+            value: {previousStatus, newStatus, workflowStatusChangeCaller: caller}
+        }
+    });
 
     window.dispatchEvent(event);
 }

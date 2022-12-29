74e4c901af1895228d6d9430a3fa79cdd5b4d221
alexisljn <alexis.lejeune@gmx.fr>
2022-12-29 08:17:35 +0100

    listen and dispatch contract event 'ProposalRegistered'

--- frontend/src/events-manager/VotingEventsManager.ts
+++ frontend/src/events-manager/VotingEventsManager.ts
@@ -5,12 +5,16 @@ function listenContractEvents(votingContract: Contract) {
     votingContract.on('VoterRegistered', voterRegisteredHandler);
 
     votingContract.on('WorkflowStatusChange', workflowStatusChangeHandler);
+
+    votingContract.on('ProposalRegistered', proposalRegisteredHandler);
 }
 
 function cleanContractEvents(votingContract: Contract) {
     votingContract.off('VoterRegistered', voterRegisteredHandler);
 
     votingContract.off('WorkflowStatusChange', workflowStatusChangeHandler);
+
+    votingContract.off('ProposalRegistered', proposalRegisteredHandler);
 }
 
 function voterRegisteredHandler(voterAddress: string, caller: string) {
@@ -29,6 +33,17 @@ function workflowStatusChangeHandler(previousStatus: number, newStatus: number, 
         detail: {
             type: 'workflowStatusChange',
             value: {previousStatus, newStatus, workflowStatusChangeCaller: caller}
+        }
+    });
+
+    window.dispatchEvent(event);
+}
+
+function proposalRegisteredHandler(id: number, caller: string) {
+    const event = new CustomEvent(CONTRACT_EVENT, {
+        detail: {
+            type: 'proposalRegistered',
+            value: {proposalRegisteredCaller: caller}
         }
     });
 
